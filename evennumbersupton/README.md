c3676cf6baf2e96226c90ae16c6f945695afa74a
Rohini Kumar Barla <rohinibarla@enhance42.com>
2024-07-03 16:48:57 +0530

    Even numbers upto n

---
+++ evennumbersupton/README.md
@@ -0,0 +1,31 @@
+## Even numbers upto N
+Print even numbers upto n.
+
+### User-experience
+```
+To display all even numbers up to a given number.
+Up to which number would you like to print even numbers? 5
+The even numbers up to 5 are 0, 2, 4.
+```
+TODO: add ascci cinema.
+
+### Correctness
+Run you program and verify that your ouput is in line with the user experience.
+
+### Run Tests
+Run the test to `verify the correctness` of your program.
+```
+check50 rohinibarla/c/problems/evennumbersupton
+```
+
+### Coding Style
+Check coding style errors
+```
+style50 evennumbersupton.c
+```
+
+### Submit
+Once all test cases pass, `submit your code`.
+```
+submit50 rohinibarla/c/problems/evennumbersupton
+```
---
+++ evennumbersupton/__init__.py
@@ -0,0 +1,40 @@
+
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """evennumbersupton.c exists"""
+    check50.exists("evennumbersupton.c")
+    check50.include("test_1_input.txt", "test_1_output.txt")
+    check50.include("test_2_input.txt", "test_2_output.txt")
+    check50.include("test_3_input.txt", "test_3_output.txt")
+
+@check50.check(exists)
+def compiles():
+    """evennumbersupton.c compiles"""
+    check50.c.compile("evennumbersupton.c", lcs50=True)
+
+
+@check50.check(compiles)
+def test_1():
+    """test_1"""
+    test_input_output("./evennumbersupton", "test_1_input.txt", "test_1_output.txt")
+
+
+@check50.check(compiles)
+def test_2():
+    """test_2"""
+    test_input_output("./evennumbersupton", "test_2_input.txt", "test_2_output.txt")
+
+
+@check50.check(compiles)
+def test_3():
+    """test_3"""
+    test_input_output("./evennumbersupton", "test_3_input.txt", "test_3_output.txt")
+
+
+# Helpers
+def test_input_output(executable, input_file, output_file):
+    """A function to test a single input/output pair"""
+    check50.run(executable).stdin(open(input_file).read(), prompt=False).stdout(open(output_file).read(), regex=False).exit()
