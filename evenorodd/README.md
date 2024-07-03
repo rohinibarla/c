53399356f3a02556bbf14192da9f4d33461d1ff7
Rohini Kumar Barla <rohinibarla@enhance42.com>
2024-07-03 06:45:23 +0530

    Even or Odd

---
+++ evenorodd/README.md
@@ -0,0 +1,37 @@
+## Even or Odd
+Check number is even or odd
+
+### User-experience
+```
+To check number is even or odd.
+Enter a number: 9
+9 is an odd number.
+```
+```
+To check number is even or odd.
+Enter a number: 42
+42 is an even number.
+```
+
+TODO: add ascci cinema.
+
+### Correctness
+Run you program and verify that your ouput is in line with the user experience.
+
+### Run Tests
+Run the test to `verify the correctness` of your program.
+```
+check50 rohinibarla/c/problems/evenorodd
+```
+
+### Coding Style
+Check coding style errors
+```
+style50 -o split evenorodd.c
+```
+
+### Submit
+Once all test cases pass, `submit your code`.
+```
+submit50 rohinibarla/c/problems/evenorodd
+```
---
+++ evenorodd/__init__.py
@@ -0,0 +1,47 @@
+
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """evenorodd.c exists"""
+    check50.exists("evenorodd.c")
+    check50.include("test_2_input.txt", "test_2_output.txt")
+    check50.include("test_1_input.txt", "test_1_output.txt")
+    check50.include("test_3_input.txt", "test_3_output.txt")
+    check50.include("test_4_input.txt", "test_4_output.txt")
+
+@check50.check(exists)
+def compiles():
+    """evenorodd.c compiles"""
+    check50.c.compile("evenorodd.c", lcs50=True)
+
+
+@check50.check(compiles)
+def test_2():
+    """test_2"""
+    test_input_output("./evenorodd", "test_2_input.txt", "test_2_output.txt")
+
+
+@check50.check(compiles)
+def test_1():
+    """test_1"""
+    test_input_output("./evenorodd", "test_1_input.txt", "test_1_output.txt")
+
+
+@check50.check(compiles)
+def test_3():
+    """test_3"""
+    test_input_output("./evenorodd", "test_3_input.txt", "test_3_output.txt")
+
+
+@check50.check(compiles)
+def test_4():
+    """test_4"""
+    test_input_output("./evenorodd", "test_4_input.txt", "test_4_output.txt")
+
+
+# Helpers
+def test_input_output(executable, input_file, output_file):
+    """A function to test a single input/output pair"""
+    check50.run(executable).stdin(open(input_file).read(), prompt=False).stdout(open(output_file).read(), regex=False).exit()
