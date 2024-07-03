9e3a62bce0dd3ecd4321e8c7c12c375e737c579c
Rohini Kumar Barla <rohinibarla@enhance42.com>
2024-07-03 16:43:32 +0530

    Print N even numbers

---
+++ nevennumbers/README.md
@@ -0,0 +1,31 @@
+## N Even numbers
+Print n even numbers
+
+### User-experience
+```
+To display a series of even numbers.
+How many even numbers would you like to see? 7
+The first 7 even numbers are 0, 2, 4, 6, 8, 10, 12.
+```
+TODO: add ascci cinema.
+
+### Correctness
+Run you program and verify that your ouput is in line with the user experience.
+
+### Run Tests
+Run the test to `verify the correctness` of your program.
+```
+check50 rohinibarla/c/problems/nevennumbers
+```
+
+### Coding Style
+Check coding style errors
+```
+style50 nevennumbers.c
+```
+
+### Submit
+Once all test cases pass, `submit your code`.
+```
+submit50 rohinibarla/c/problems/nevennumbers
+```
---
+++ nevennumbers/__init__.py
@@ -0,0 +1,40 @@
+
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """nevennumbers.c exists"""
+    check50.exists("nevennumbers.c")
+    check50.include("test_1_input.txt", "test_1_output.txt")
+    check50.include("test_2_input.txt", "test_2_output.txt")
+    check50.include("test_3_input.txt", "test_3_output.txt")
+
+@check50.check(exists)
+def compiles():
+    """nevennumbers.c compiles"""
+    check50.c.compile("nevennumbers.c", lcs50=True)
+
+
+@check50.check(compiles)
+def test_1():
+    """test_1"""
+    test_input_output("./nevennumbers", "test_1_input.txt", "test_1_output.txt")
+
+
+@check50.check(compiles)
+def test_2():
+    """test_2"""
+    test_input_output("./nevennumbers", "test_2_input.txt", "test_2_output.txt")
+
+
+@check50.check(compiles)
+def test_3():
+    """test_3"""
+    test_input_output("./nevennumbers", "test_3_input.txt", "test_3_output.txt")
+
+
+# Helpers
+def test_input_output(executable, input_file, output_file):
+    """A function to test a single input/output pair"""
+    check50.run(executable).stdin(open(input_file).read(), prompt=False).stdout(open(output_file).read(), regex=False).exit()
