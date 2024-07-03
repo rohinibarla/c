b4dfd9c509cf0d7bc54bfc55d7944830f06f0915
Rohini Kumar Barla <rohinibarla@enhance42.com>
2024-07-03 16:23:06 +0530

    natural numbers upto n

---
+++ natural/README.md
@@ -0,0 +1,29 @@
+## L#10.01.Tutorial: Natural numbers
+
+
+### User-experience
+```
+
+```
+TODO: add ascci cinema.
+
+### Correctness
+Run you program and verify that your ouput is in line with the user experience.
+
+### Run Tests
+Run the test to `verify the correctness` of your program.
+```
+check50 rohinibarla/c/problems/natural
+```
+
+### Coding Style
+Check coding style errors
+```
+style50 -o split natural.c
+```
+
+### Submit
+Once all test cases pass, `submit your code`.
+```
+submit50 rohinibarla/c/problems/natural
+```
---
+++ natural/__init__.py
@@ -0,0 +1,47 @@
+
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """natural.c exists"""
+    check50.exists("natural.c")
+    check50.include("test_1_input.txt", "test_1_output.txt")
+    check50.include("test_4_input.txt", "test_4_output.txt")
+    check50.include("test_2_input.txt", "test_2_output.txt")
+    check50.include("test_3_input.txt", "test_3_output.txt")
+
+@check50.check(exists)
+def compiles():
+    """natural.c compiles"""
+    check50.c.compile("natural.c", lcs50=True)
+
+
+@check50.check(compiles)
+def test_1():
+    """test_1"""
+    test_input_output("./natural", "test_1_input.txt", "test_1_output.txt")
+
+
+@check50.check(compiles)
+def test_4():
+    """test_4"""
+    test_input_output("./natural", "test_4_input.txt", "test_4_output.txt")
+
+
+@check50.check(compiles)
+def test_2():
+    """test_2"""
+    test_input_output("./natural", "test_2_input.txt", "test_2_output.txt")
+
+
+@check50.check(compiles)
+def test_3():
+    """test_3"""
+    test_input_output("./natural", "test_3_input.txt", "test_3_output.txt")
+
+
+# Helpers
+def test_input_output(executable, input_file, output_file):
+    """A function to test a single input/output pair"""
+    check50.run(executable).stdin(open(input_file).read(), prompt=False).stdout(open(output_file).read(), regex=False).exit()
